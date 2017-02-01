fe2413b0284d5b3e22be35b7e16ad20f106d8f24
Kelly Morrison <kelly@semantria.com>
2017-02-01 23:15:10 +0000

    added email signup screen

--- SousChef/EmailSignupVC.swift
+++ SousChef/EmailSignupVC.swift
@@ -10,32 +10,57 @@ import UIKit
 
 class EmailSignupVC: UIViewController {
 
-    @IBOutlet weak var enterEmail: UITextField!
-    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
+       
         super.viewDidLoad()
-        let attributes = [
-            NSForegroundColorAttributeName: UIColor.darkGray,
-            NSFontAttributeName : UIFont(name: "Arial", size: 20)! //TODO: update to simplifica
-        ]
+        self.hideKeyboardWhenTappedAround()
+        // Note that SO highlighting makes the new selector syntax (#selector()) look
+        // like a comment but it isn't one
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardNotification(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
         
-        enterEmail.attributedPlaceholder = NSAttributedString(string: "Enter Email", attributes:attributes)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //TODO: Fix fonts of placeholder text
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
-    */
+    
+    func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let duration:TimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIViewAnimationOptions.curveEaseInOut.rawValue
+            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+            if (endFrame?.origin.y)! >= UIScreen.main.bounds.size.height {
+                self.logInConstraint?.constant = 38.0
+            } else {
+                self.logInConstraint?.constant = endFrame?.size.height ?? 0.0
+            }
+            UIView.animate(withDuration: duration,
+                           delay: TimeInterval(0),
+                           options: animationCurve,
+                           animations: { self.view.layoutIfNeeded() },
+                           completion: nil)
+        }
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(EmailSignupVC.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @IBAction func backPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil);
+    }
+
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
 
 }
