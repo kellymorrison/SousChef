7eb1aaef2219372b898a9936878867935183be59
Kelly Morrison <kelly@semantria.com>
2017-02-06 22:02:20 +0000

    added facebook authentication

--- SousChef/AppDelegate.swift
+++ SousChef/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Firebase
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +19,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+
         return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?,
+                     annotation: Any) -> Bool {
+        let handled: Bool = FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+        // Add any custom logic here.
+        return handled
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -36,6 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- SousChef/SignupVC.swift
+++ SousChef/SignupVC.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKLoginKit
+import FirebaseAuth
 
-class SignupVC: UIViewController {
+class SignupVC: UIViewController,FBSDKLoginButtonDelegate {
 
-    override func viewDidLoad() {
+    var facebookSignupButton: FBSDKLoginButton = FBSDKLoginButton()
+    
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if FIRAuth.auth()?.currentUser != nil {
+            // User is signed in.
+            // move the user to the home screen
+            print("USER LOGGED INNNNN")
+            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let homeVC: UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "HomeVC")
+            
+            self.present(homeVC, animated: true, completion: nil)
+            
+        } else {
+            
+            facebookSignupButton.center = self.view.center
+            self.facebookSignupButton.readPermissions = ["public_profile", "email", "user_friends"]
+            self.facebookSignupButton.delegate = self
+            
+            self.view!.addSubview(facebookSignupButton)
+        }
+
+        
+        
+        
+//        let reference = FIRDatabase.database().reference()
+//        reference.child("NewChild").setValue("new child value")
+//        reference.child("newArry").setValue(["this","is","an","array"])
+//        // LMK when a new child is added / what is in the database
+//        reference.observe(.childAdded, with: { (snapshot) in
+//            print("THIS IS WHAT IS IN THE DATABASE \(snapshot)")
+//        
+//        })
+//        //adds a new child and automatically generates key
+//        reference.child("giveMeKeys").childByAutoId().setValue("Test")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +56,17 @@ class SignupVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("user logged in")
+        let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
 
+        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+             print("user logged in with Firebase")
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("user logged out")
+    }
 }
 
